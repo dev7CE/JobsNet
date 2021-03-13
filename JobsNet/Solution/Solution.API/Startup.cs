bf74fdbcd78ca80608bc3ec43a3d1c1530f3c005
ByronLeal <dev7CE@hotmail.com>
2021-03-13 17:34:39 -0600

    Feat: Swagger API Documentation

--- JobsNet/Solution/Solution.API/Startup.cs
+++ JobsNet/Solution/Solution.API/Startup.cs
@@ -23,11 +23,25 @@ namespace Solution.API
             services.AddDbContext<SolutionDbContext>(options => 
                 options.UseSqlServer(Configuration.GetConnectionString("JobsNetConnString")));
             services.AddControllers();
+
+            // SWAGGER API Documentation
+            services.AddSwaggerGen();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            // SWAGGER API Documentation
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
+            
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
