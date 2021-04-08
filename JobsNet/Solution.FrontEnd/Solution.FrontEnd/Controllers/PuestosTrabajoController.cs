163bd06ddbe7ee41452a6bc89a64e40b8e074e29
ByronLeal <dev7CE@hotmail.com>
2021-04-07 23:31:29 -0600

    Feat: (Empleador) Aniadir Puesto de Trabajo

--- JobsNet/Solution.FrontEnd/Solution.FrontEnd/Controllers/PuestosTrabajoController.cs
+++ JobsNet/Solution.FrontEnd/Solution.FrontEnd/Controllers/PuestosTrabajoController.cs
@@ -8,6 +8,7 @@ using Newtonsoft.Json;
 using System.Linq;
 using Solution.FrontEnd.Models;
 using Microsoft.Extensions.Options;
+using System.Text;
 
 namespace Solution.FrontEnd.Controllers
 {
@@ -17,8 +18,13 @@ namespace Solution.FrontEnd.Controllers
         public PuestosTrabajoController() { }
         //
         // GET: PuestosTrabajo
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(ControllerMessageId? message = null)
         {
+            ViewData["StatusMessage"] =
+                message == ControllerMessageId.AddPuestoTrabajoSuccess ? "Se ha agregado el puesto."
+                : message == ControllerMessageId.Error ? "Ha ocurrido un error."
+                : "";
+
             List<data.PuestosTrabajo> aux = new List<data.PuestosTrabajo>();
             using (var client = new HttpClient())
             {
@@ -37,6 +43,31 @@ namespace Solution.FrontEnd.Controllers
             }
             return View(GetByUserName(aux));
         }
+        //
+        // GET: PuestosTrabajo/Create
+        public IActionResult Create()
+        {
+            //ViewData["IdEmpresa"] = new SelectList(_context.Empresas, "IdEmpresa", "NombreEmpresa");
+            return View();
+        }
+        //
+        // POST: PuestosTrabajo/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("IdPuesto,Titulo,Descripcion,Requisitos,FechaCierre")] PuestosTrabajo puestosTrabajo)
+        {
+            puestosTrabajo.IdEmpresa = GetEmpresaByUserName().Result.IdEmpresa;
+            if (ModelState.IsValid)
+            {
+                if(await CreatePuesto(puestosTrabajo))
+                return RedirectToAction(nameof(Index), new { Message = ControllerMessageId.AddPuestoTrabajoSuccess });
+
+                return RedirectToAction(nameof(Index), new { Message = ControllerMessageId.Error });
+            }
+            return View(puestosTrabajo);
+        }
         #region Helpers
         private IEnumerable<data.PuestosTrabajo> GetByUserName (IEnumerable<data.PuestosTrabajo> list)
         {
@@ -44,6 +75,61 @@ namespace Solution.FrontEnd.Controllers
                 e.Empresa.UserName.Equals(User.Identity.Name))
                 .OrderByDescending(e => e.FechaCierre)
                 .ToList();
+        }
+
+        private async Task<data.Empresas> GetEmpresaByUserName()
+        {
+            // Here is the code
+            IEnumerable<data.Empresas> e = await GetEmpresas();
+            return e.SingleOrDefault(e => 
+                e.UserName.Equals(User.Identity.Name)
+            );
+        }
+        private async Task<IEnumerable<data.Empresas>> GetEmpresas ()
+        {
+            List<data.Empresas> aux = new List<data.Empresas>();
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(baseurl);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(
+                    new System.Net.Http.Headers
+                        .MediaTypeWithQualityHeaderValue("application/json"));
+                HttpResponseMessage res = await client.GetAsync("api/Empresas");
+
+                if (res.IsSuccessStatusCode)
+                {
+                    var auxres = res.Content.ReadAsStringAsync().Result;
+                    aux = JsonConvert.DeserializeObject<List<data.Empresas>>(auxres);
+                }
+            }
+            return aux;
+        }
+
+        private async Task<bool> CreatePuesto(data.PuestosTrabajo model)
+        {
+            using (var client = new HttpClient())
+            {
+                var requestContent = new StringContent(
+                    JsonConvert.SerializeObject(model), 
+                    Encoding.UTF8, 
+                    "application/json"
+                );
+
+                client.BaseAddress = new Uri(baseurl);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(
+                    new System.Net.Http.Headers
+                        .MediaTypeWithQualityHeaderValue("application/json"));
+                HttpResponseMessage res = await client
+                    .PostAsync("api/PuestosTrabajo", requestContent);
+                return res.IsSuccessStatusCode;
+            }
+        }
+        public enum ControllerMessageId
+        {
+            AddPuestoTrabajoSuccess,
+            Error
         }
         #endregion
     }
--- JobsNet/Solution.FrontEnd/Solution.FrontEnd/Models/PuestosTrabajo.cs
+++ JobsNet/Solution.FrontEnd/Solution.FrontEnd/Models/PuestosTrabajo.cs
@@ -14,9 +14,9 @@ namespace Solution.FrontEnd.Models
 
         public int IdPuesto { get; set; }
         public int IdEmpresa { get; set; }
-        [Required]
+        [Required (ErrorMessage = "Este campo es requerido")]
         [StringLength(150)]
-        [Display(Name = "Título")]
+        [Display(Name = "Título **")]
         public string Titulo { get; set; }
         [StringLength(150)]
         [Display(Name = "Descripción")]
@@ -25,7 +25,8 @@ namespace Solution.FrontEnd.Models
         public string Requisitos { get; set; }
         [Display(Name = "Fecha de Publicación")]
         public DateTime? FechaPublicacion { get; set; }
-        [Display(Name = "Fecha de Cierre")]
+        [Required (ErrorMessage = "Este campo es requerido")]
+        [Display(Name = "Fecha de Cierre  **")]
         public DateTime FechaCierre { get; set; }
 
         public virtual Empresas Empresa { get; set; }
