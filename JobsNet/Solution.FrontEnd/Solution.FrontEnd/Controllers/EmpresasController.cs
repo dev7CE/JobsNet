08ed3eec1295aab0bd8f236836cd5f9c198dda21
ByronLeal <dev7CE@hotmail.com>
2021-04-10 17:44:43 -0600

    Feat: (Empresas & Oferentes) Seccion de Perfil

---
+++ JobsNet/Solution.FrontEnd/Solution.FrontEnd/Controllers/EmpresasController.cs
@@ -0,0 +1,47 @@
+﻿using data = Solution.FrontEnd.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net.Http;
+using Newtonsoft.Json;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Solution.FrontEnd.Controllers
+{
+    [Authorize]
+    public class EmpresasController : Controller
+    {
+        private readonly string baseurl = "http://localhost:5000/";
+        public EmpresasController() { }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await GetEmpresaByUserName());
+        }
+        #region Helpers
+        public async Task<data.Empresas> GetEmpresaByUserName ()
+        {
+            string userName = User.Identity.Name; 
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(baseurl);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(
+                    new System.Net.Http.Headers
+                        .MediaTypeWithQualityHeaderValue("application/json"));
+                HttpResponseMessage res = await client.GetAsync("api/Empresas");
+        
+                if (res.IsSuccessStatusCode)
+                {
+                    var auxres = res.Content.ReadAsStringAsync().Result;
+                    return JsonConvert.DeserializeObject<IEnumerable<data.Empresas>>(auxres)
+                        .SingleOrDefault(o => o.UserName.Equals(userName));
+                }
+                return null;
+            }
+        }
+        #endregion
+    }
+}
